f68753152ddbbc8d782fd400c31b956fd64f6bb4
Ouyang <250757958@qq.com>
2016-12-14 23:15:09 +0800

    查看用户

--- web/pages/user/user.js
+++ web/pages/user/user.js
@@ -1,6 +1,7 @@
 /**
  * Created by OuyangDuning on 2016/9/28.
  */
+var grid = null;
 
 var initPage = function () {
     initStyle();
@@ -43,11 +44,49 @@ var initPageData = function () {
         data:null,
         dataType:'json',
         success:function (data) {
-            console.log(data);
+            // drawTable(data);
+            for (var i = 0; i < data.length; i ++) {
+                data[i].no = i + 1;
+                data[i].time = new Date(data[i]["createDate"].time).format("yyyy-MM-dd")
+            }
+            if (grid) {
+                myJqGrid.reloadGrid(data);
+            } else {
+                myJqGrid.initJqGrid(data);
+            }
         }
     })
 };
 
+var myJqGrid = {
+    initJqGrid : function (data) {
+        grid = new crm.grid.createJqGrid("userList");//获得页面上的table对象
+        grid.setHeight($(window).height() - 330);//设置table高度
+        grid.setTHead(['','序号','登录账号','用户名','电话', '邮箱','创建时间']);//设置table的head
+        grid.addColModel({name:'userId',index:'userId',hidden:true,key:true});
+        grid.addColModel({name:'no',index:'no',sortable:false,width:25});
+        grid.addColModel({name:'userCode',index:'userCode',sortable:false,width:60});
+        // grid.addColModel({name:'roleName',index:'roleName',sortable:false,width:90});
+        grid.addColModel({name:'userName',index:'userName',sortable:false,width:90});
+        grid.addColModel({name:'userPhone',index:'userPhone',sortable:false,width:150});
+        grid.addColModel({name:'userEmail',index:'userEmail',sortable:false,width:70});
+        grid.addColModel({name:'time',index:'time',sortable:false,width:70});
+        grid.setTableAttribute({onSelectRow:function(rowId){checkAllSelectRow();}});
+        grid.loadDataForJqGrid(data);
+
+    },
+    reloadGrid : function (data) {
+        grid.getJqGrid().clearGridData();
+        // grid.setPageSize(page.getPageSize());
+        grid.reloadGrid(data);
+        // grid.refreshNumber(page.getStart(), "number");
+    }
+};
+
+/**
+ * 新增用户界面
+ * @param url 新增页面地址
+ */
 var drawAddPage = function (url) {
     var addPath = path + url;
     $("#page-content").load(addPath);
